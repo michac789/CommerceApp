cd466c0db00e0086dd3a9b95ca10f6ce1139e7d8
unknown <kristianachwan@gmail.com>
2022-07-28 21:23:11 +0800

    cart 'finished'

--- catalog/static/catalog/index.js
+++ catalog/static/catalog/index.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     let cards = document.querySelectorAll(".card");
 
-cards.forEach(card =>  
-    card.addEventListener("mouseover", () => 
-        card.style.border = null
+    cards.forEach(card =>  
+        card.addEventListener("mouseover", () => 
+            card.style.border = null
+        )
+
     )
 
-)
 
+    cards.forEach(card =>  
+        card.addEventListener("mouseout", () => 
+            card.style.border = "hidden"
+        )
 
-cards.forEach(card =>  
-    card.addEventListener("mouseout", () => 
-        card.style.border = "hidden"
     )
-
-)
 
 })
