0b9909c92ed22657c13e36795f2feab5963d29a8
unknown <kristianachwan@gmail.com>
2022-07-30 02:33:21 +0800

    fixed wrong js attached

--- catalog/static/catalog/item.js
+++ catalog/static/catalog/item.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const itemID = window.location.pathname.split('/')[2]
     const addCartButtonReference = document.querySelector('.add-cart-btn') 
     const addedMessageReference = document.querySelector('.added-message') 
-    // Why error? 
+    // Why error? while there's error checking there in DOMContentLoaded :(
     if ( !addCartButtonReference ) { 
         addCartButtonReference.addEventListener('click', () => {
             fetch (`/api/cart/${itemID}`, {method: 'PUT'})
