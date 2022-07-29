e4d5f3ba5a7008400d3c3b27ebfd8570c5be3e3b
unknown <kristianachwan@gmail.com>
2022-07-30 02:09:28 +0800

    finishing delete feature in cart, along with adding item in cart from item

--- cart/static/cart/index.js
+++ cart/static/cart/index.js
@@ -16,18 +16,43 @@ document.addEventListener('DOMContentLoaded', () => {
                 ` 
                 data.forEach(item => 
                 cartReference.innerHTML +=
-                    `<li class="list-group-item d-flex justify-content-between lh-sm">
+                    `<li class="list-group-item d-flex justify-content-between lh-sm cart-item-${item.item_id}">
                     <div>
-                        <h6 class="my-0">${item.item_title}</h6>
-                        <small class="text-muted"></small>
+                        <h6 class="my-0">
+                            <a class="text-secondary" href="../catalog/${item.item_id}">
+                                ${item.item_title}
+                            </a>
+
+                            <button  data-item-id="${item.item_id}" class="remove-cart-${item.item_id}-btn btn remove-btn"><i class="bi bi-trash"></i></button>
+                            </h6> 
+                            <small class="text-muted"></small> 
                     </div>
                     <span class="text-muted">$13</span>
                     </li>` 
-                
                 )
+                const itemID = document.querySelectorAll('button.remove-btn').forEach((button) => {
+                    button.addEventListener('click', () => {
+                        fetch (`/api/cart/${button.dataset.itemId}`, {method : 'DELETE'})
+                            .then(response => response.json())
+                            .then(data => 
+                                $(`<div class="alert alert-warning container my-3">Item removed from cart...</div>`).insertAfter(".cart-container-wrapper")
+                                )
+                            .then(() => {
+                                const deletedItem = document.querySelector(`.cart-item-${button.dataset.itemId}`) 
+                                deletedItem.remove()
+                            })
+                    })
+                })
+
+
+
             }
         }
         )
+      
+
+
+        
 })
 
 // 
