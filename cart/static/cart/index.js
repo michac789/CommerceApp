c2b0ecc8797a08389134e7d115f39ce25fde7bbb
unknown <kristianachwan@gmail.com>
2022-07-29 15:33:42 +0800

    cart done

--- cart/static/cart/index.js
+++ cart/static/cart/index.js
@@ -1,17 +1,35 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => { 
     const cartReference = document.querySelector('.cart')  
-
+    const itemCounterReference = document.querySelector('span.item-counter') 
+    const itemCounterWrapperReference = document.querySelector('h4.item-counter-wrapper') 
     fetch('/api/cart/1')
         .then(response => response.json())
-        .then(data => data.forEach(item => 
-            cartReference.innerHTML +=
-                `<li class="list-group-item d-flex justify-content-between lh-sm">
-                <div>
-                    <h6 class="my-0">${item.item_title}</h6>
-                    <small class="text-muted"></small>
-                </div>
-                <span class="text-muted">$13</span>
-                </li>`
-            )
+        .then(data => {
+            console.log(data)
+            if(data.length == 0) { 
+                itemCounterWrapperReference.innerHTML += 
+                    '<p class="lead fw-300 pt-5">Your cart is empty...</p>'  
+            } 
+            else { 
+                itemCounterWrapperReference.innerHTML = `
+                    <span class="text-primary">Your cart</span>
+                    <span class="badge bg-primary rounded-pill item-counter ms-3">${data.length}</span>
+                ` 
+                data.forEach(item => 
+                cartReference.innerHTML +=
+                    `<li class="list-group-item d-flex justify-content-between lh-sm">
+                    <div>
+                        <h6 class="my-0">${item.item_title}</h6>
+                        <small class="text-muted"></small>
+                    </div>
+                    <span class="text-muted">$13</span>
+                    </li>` 
+                
+                )
+            }
+        }
         )
 })
+
+// 
+//                 
